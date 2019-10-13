6530dd9a4da0663de674ee8ff92da38b32eb40d9
Vitali Mkala <vmakal1989@gmail.com>
2019-10-13 12:06:34 +0300

    create components Heder,Navbar,Profile

---
+++ src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="http://we.com.mk/wp-content/uploads/2016/12/greece-islands.jpg" />
+      </div>
+      <div>
+        ava + description
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            <div>
+              Post 1
+            </div>
+            <div>
+              Post 2
+            </div>
+            <div>
+              Post 3
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>  
+    )
+    }
+    
+export default Profile;

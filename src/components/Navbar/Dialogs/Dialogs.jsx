c1927645cfb984c1e4b743a29a1d16a81b1e7ef9
Vitali Mkala <vmakal1989@gmail.com>
2019-10-15 16:31:39 +0300

    Lesson 22

--- src/components/Navbar/Dialogs/Dialogs.jsx
+++ src/components/Navbar/Dialogs/Dialogs.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import style from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
         <div className={style.dialogsBody}>
             <div className={style.dialogs}>
                 <div>
-                  Artur
+                    <NavLink to='/dialogs/id1' activeClassName={style.active}>Artur</NavLink>
                 </div>
                 <div>
-                    Valera
+                    <NavLink to='/dialogs/id2' activeClassName={style.active}>Valera</NavLink>
                 </div>
                 <div>
-                    Kris
+                    <NavLink to='/dialogs/id3' activeClassName={style.active}>Kristina</NavLink>
                 </div>
             </div>
             <div className={style.comments}>

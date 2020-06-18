75f369e01b9e0fa0f49e0b788c25b621ba4655ba
v.makal <vmakal1989@gmail.com>
2020-06-18 09:02:42 +0300

    Updates

---
+++ precache-manifest.76526ddd817c715db3705a9b5bb7aa53.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f75d258af819a1f78f7e78f1870cad0",
+    "url": "/vmakal1989/it-kamasutra/index.html"
+  },
+  {
+    "revision": "98b0981a59a6d3f111b0",
+    "url": "/vmakal1989/it-kamasutra/static/css/3.659f6396.chunk.css"
+  },
+  {
+    "revision": "961a4059709c2d0bdb53",
+    "url": "/vmakal1989/it-kamasutra/static/css/4.70cd701b.chunk.css"
+  },
+  {
+    "revision": "c7cdff43a03df148be07",
+    "url": "/vmakal1989/it-kamasutra/static/css/main.013fd987.chunk.css"
+  },
+  {
+    "revision": "b13a39299214b6317c9d",
+    "url": "/vmakal1989/it-kamasutra/static/js/2.cf2854e3.chunk.js"
+  },
+  {
+    "revision": "98b0981a59a6d3f111b0",
+    "url": "/vmakal1989/it-kamasutra/static/js/3.d15c51c3.chunk.js"
+  },
+  {
+    "revision": "961a4059709c2d0bdb53",
+    "url": "/vmakal1989/it-kamasutra/static/js/4.74938466.chunk.js"
+  },
+  {
+    "revision": "c7cdff43a03df148be07",
+    "url": "/vmakal1989/it-kamasutra/static/js/main.edcf2032.chunk.js"
+  },
+  {
+    "revision": "c0b01b8cd1844bf566c4",
+    "url": "/vmakal1989/it-kamasutra/static/js/runtime-main.c426f2f5.js"
+  },
+  {
+    "revision": "728f9b4d00b8c8443987639ed2fbbe1e",
+    "url": "/vmakal1989/it-kamasutra/static/media/preloader.728f9b4d.gif"
+  },
+  {
+    "revision": "f8a15e74d9b85e04019c7efd5ae76dff",
+    "url": "/vmakal1989/it-kamasutra/static/media/user.f8a15e74.png"
+  }
+]);
